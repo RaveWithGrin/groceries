c789e3788b1194fb2afe7c09c53442ab8d264eb6
RaveWithGrin <ryanseanmillward@gmail.com>
2019-04-18 21:42:21 -0400

    Get the flyer id as it changes weekly

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var request = require('request-promise');
+var cheerio = require('cheerio');
 
 var getGroceries = async function(store) {
     var options = {
-        url: store.url,
+        url: store.flyerURL + store.flyerId,
         headers: {
             Accept: '*/*'
         }
@@ -42,36 +43,66 @@ var getGroceries = async function(store) {
     return groceries;
 };
 
+var getFlyerId = async function(url) {
+    var flyerId = undefined;
+    var options = {
+        url: url,
+        headers: {
+            Accept: '*/*'
+        }
+    };
+    var response = await request.get(options);
+    var html = cheerio.load(response);
+    var regexp = /current_flyer_id":(.+?),/g;
+    html('script')
+        .get()
+        .forEach(function(script) {
+            script.children.forEach(function(child) {
+                if (child.data.indexOf('current_flyer_id') !== -1) {
+                    flyerId = regexp.exec(child.data)[1];
+                }
+            });
+        });
+    return flyerId;
+};
+
 var main = async function() {
     var stores = [
         {
             name: 'No Frills',
-            url: 'http://flyers.nofrills.ca/flyer_data/2496238?locale=en'
+            flyerIdURL: 'http://flyers.nofrills.ca/flyers/nofrills?type=2&store_code=3123',
+            flyerURL: 'http://flyers.nofrills.ca/flyer_data/'
         },
         {
             name: 'Food Basics',
-            url: 'http://ecirculaire.foodbasics.ca/flyer_data/2483336?locale=en'
+            flyerIdURL: 'http://ecirculaire.foodbasics.ca/flyers/foodbasics?type=2&store_code=937',
+            flyerURL: 'http://ecirculaire.foodbasics.ca/flyer_data/'
         },
         {
             name: 'Metro',
-            url: 'http://ecirculaire.metro.ca/flyer_data/2486720?locale=en'
+            flyerIdURL: 'http://ecirculaire.metro.ca/flyers/metro?type=2&store_code=4460',
+            flyerURL: 'http://ecirculaire.metro.ca/flyer_data/'
         },
         {
             name: 'Fortinos',
-            url: 'http://flyers.fortinos.ca/flyer_data/2487869?locale=en'
+            flyerIdURL: 'http://flyers.fortinos.ca/flyers/fortinos-flyer?type=2&store_code=2265',
+            flyerURL: 'http://flyers.fortinos.ca/flyer_data/'
         },
         {
             name: 'Lococos',
-            url: 'http://flyer.lococos.ca/flyer_data/2464276?locale=en'
+            flyerIdURL: 'http://flyer.lococos.ca/flyers/lococos?type=2&store_code=2',
+            flyerURL: 'http://flyer.lococos.ca/flyer_data/'
         },
         {
             name: 'Walmart',
-            url: 'http://flyers.walmart.ca/flyer_data/2486735?locale=en'
+            flyerIdURL: 'https://flyers.walmart.ca/flyers/walmartcanada?type=2&store_code3037',
+            flyerURL: 'http://flyers.walmart.ca/flyer_data/'
         }
     ];
     var groceries = {};
     for (var i = 0; i < stores.length; i++) {
         var store = stores[i];
+        store.flyerId = await getFlyerId(store.flyerIdURL);
         groceries[store.name] = await getGroceries(store);
     }
     console.log(JSON.stringify(groceries, null, 2));
